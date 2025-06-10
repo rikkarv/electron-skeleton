62f5d18617c38a2224613c135e0730e06406e13c
Ricardo Carvalho <rikardokarvalho@hotmail.com>
2025-06-10 17:11:53 +0100

    add custom bar

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 
 const createWindow = () => {
     const window = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        },
+        frame: false
     });
 
     window.loadFile('index.html');
@@ -15,4 +20,12 @@ app.whenReady().then(() => {
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
+});
+
+ipcMain.on('window-control', (event, action) => {
+  const win = BrowserWindow.getFocusedWindow();
+  if (!win) return;
+  if (action === 'minimize') win.minimize();
+  if (action === 'maximize') win.isMaximized() ? win.unmaximize() : win.maximize();
+  if (action === 'close') win.close();
 });
